c9b23555f48eea0e7ea8bd5582be7d5cd710e4a4
Alan Griffiths <alan@octopull.co.uk>
2015-03-13 15:33:18 +0000

    Surface::query() goes const

--- tests/modules/common/mock_surface.h
+++ tests/modules/common/mock_surface.h
@@ -79,7 +79,7 @@ struct MockSurface : public mir::scene::Surface
     MOCK_CONST_METHOD0(supports_input, bool());
     MOCK_CONST_METHOD0(client_input_fd, int());
     MOCK_METHOD2(configure, int(MirSurfaceAttrib attrib, int value));
-    MOCK_METHOD1(query, int(MirSurfaceAttrib attrib));
+    MOCK_CONST_METHOD1(query, int(MirSurfaceAttrib attrib));
 
     // from mir::scene::SurfaceBufferAccess
     MOCK_METHOD1(with_most_recent_buffer_do, void(std::function<void(graphics::Buffer&)> const& exec));
