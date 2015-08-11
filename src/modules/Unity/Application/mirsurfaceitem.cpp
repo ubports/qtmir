ee2543285a3b48e6a698731da4b6aa90f9045294
Gerry Boland <gerry.boland@canonical.com>
2015-08-11 13:08:32 +0100

    Standardize licences to LGPLv3, update years, remove authors

--- src/modules/Unity/Application/mirsurfaceitem.cpp
+++ src/modules/Unity/Application/mirsurfaceitem.cpp
@@ -12,10 +12,6 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Authors:
- *     Daniel d'Andrada <daniel.dandrada@canonical.com>
- *     Gerry Boland <gerry.boland@canonical.com>
  */
 
 // local
--- tests/modules/MirSurfaceItem/mirsurfaceitem_test.cpp
+++ tests/modules/MirSurfaceItem/mirsurfaceitem_test.cpp
@@ -12,7 +12,6 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 #define MIR_INCLUDE_DEPRECATED_EVENT_HEADER
