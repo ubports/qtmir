afa081e75347ffbc835a00a3fe43884a8e068c80
Albert Astals Cid <albert.astals@canonical.com>
2015-09-30 11:38:17 +0200

    More compile fixes

--- src/modules/Unity/Application/objectlistmodel.h
+++ src/modules/Unity/Application/objectlistmodel.h
@@ -78,7 +78,7 @@ public:
         return QVariant();
     }
 
-    QHash<int, QByteArray> roleNames() const
+    QHash<int, QByteArray> roleNames() const override
     {
         QHash<int, QByteArray> roleNames;
         roleNames.insert(RoleModelData, "modelData");
