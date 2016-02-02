5924695ca9a7ab629d872bf91c73bbc0263d35c0
Gerry Boland <gerry.boland@canonical.com>
2016-02-02 17:46:52 +0000

    Implement a more sensible MirDisplayConfigurationPolicy to suit pocket desktop

--- src/platforms/mirserver/mirdisplayconfigurationpolicy.cpp
+++ src/platforms/mirserver/mirdisplayconfigurationpolicy.cpp
@@ -19,15 +19,36 @@
 #include <mir/graphics/display_configuration.h>
 #include <mir/geometry/point.h>
 
+#include <qglobal.h>
+#include <QByteArray>
+
 namespace mg = mir::graphics;
+
+#define ENV_GRID_UNIT_PX "GRID_UNIT_PX"
+#define DEFAULT_GRID_UNIT_PX 8
+
+namespace {
+static float getenvFloat(const char* name, float defaultValue)
+{
+    QByteArray stringValue = qgetenv(name);
+    bool ok;
+    float value = stringValue.toFloat(&ok);
+    return ok ? value : defaultValue;
+}
+} //namespace
 
 MirDisplayConfigurationPolicy::MirDisplayConfigurationPolicy(
         const std::shared_ptr<mir::graphics::DisplayConfigurationPolicy> &wrapped)
     : m_wrapped(wrapped)
 {
+    float gridUnit = DEFAULT_GRID_UNIT_PX;
+    if (qEnvironmentVariableIsSet(ENV_GRID_UNIT_PX)) {
+        gridUnit = getenvFloat(ENV_GRID_UNIT_PX, DEFAULT_GRID_UNIT_PX);
+    }
+    m_defaultScale = gridUnit / DEFAULT_GRID_UNIT_PX;
 }
 
-void MirDisplayConfigurationPolicy::apply_to(mg::DisplayConfiguration& conf)
+void MirDisplayConfigurationPolicy::apply_to(mg::DisplayConfiguration &conf)
 {
     int nextTopLeftPosition = 0;
 
@@ -35,21 +56,42 @@ void MirDisplayConfigurationPolicy::apply_to(mg::DisplayConfiguration& conf)
 
     //TODO: scan through saved configurations and select matching one to apply
 
+    // if LVDS panel, guess it is a phone/tablet
+    bool phoneDetected = false;
+    int screenCount = 0;
     conf.for_each_output(
-        [&](mg::UserDisplayConfigurationOutput& output)
+        [&](const mg::DisplayConfigurationOutput &output )
         {
             if (output.connected && output.used) {
-                output.top_left = mir::geometry::Point{nextTopLeftPosition, 0};
-                nextTopLeftPosition += output.modes[output.preferred_mode_index].size.width.as_int();
+                screenCount++;
+
+                if (output.type == mg::DisplayConfigurationOutputType::lvds) {
+                    phoneDetected = true;
+                }
+            }
+        });
+
+    conf.for_each_output(
+        [&](mg::UserDisplayConfigurationOutput &output)
+        {
+            if (!output.connected || !output.used) {
+                return;
             }
 
-            if (output.type == mg::DisplayConfigurationOutputType::hdmia
-                    || output.type == mg::DisplayConfigurationOutputType::hdmib) {
+            output.top_left = mir::geometry::Point{nextTopLeftPosition, 0};
+            nextTopLeftPosition += output.modes[output.preferred_mode_index].size.width.as_int();
+
+            if (phoneDetected) {
+                if (screenCount == 1 || output.type == mg::DisplayConfigurationOutputType::lvds) {
+                    output.form_factor = mir_form_factor_phone;
+                    output.scale = m_defaultScale;
+                } else { // screenCount > 1 && output.type != lvds
+                    output.form_factor = mir_form_factor_monitor;
+                    output.scale = 1;
+                }
+            } else { // desktop
                 output.form_factor = mir_form_factor_monitor;
-                output.scale = 2;
-            } else {
-                output.form_factor = mir_form_factor_phone;
-                output.scale = 1;
+                output.scale = m_defaultScale; // probably 1 on desktop anyway.
             }
         });
 }
