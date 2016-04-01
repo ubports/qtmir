36eec632b94fe0d5ec793918a6a81f344ab2c298
Robert Bruce Park <robert.park@canonical.com>
2016-03-31 17:50:01 -0700

    Initial fix for -gles test.

--- tests/convert-to-gles.sh
+++ tests/convert-to-gles.sh
@@ -5,6 +5,9 @@ set -eux
 QUILT_EXIT=0
 export QUILT_PATCHES=debian/gles-patches
 
+# Ensure we start from a clean slate, even in the -gles package.
+patch --strip=1 --reverse <debian/gles-patches/convert-to-gles.patch || true
+
 quilt push -a || QUILT_EXIT=$?
 quilt pop -a
 
