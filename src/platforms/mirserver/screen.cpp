d04bb80df13c0961cacddca0713f3338bd2d9b9e
Gerry Boland <gerry.boland@canonical.com>
2016-04-26 11:56:31 +0200

    Fix FTBFS

--- src/platforms/mirserver/screen.cpp
+++ src/platforms/mirserver/screen.cpp
@@ -142,14 +142,14 @@ bool Screen::skipDBusRegistration = false;
 
 Screen::Screen(const mir::graphics::DisplayConfigurationOutput &screen)
     : QObject(nullptr)
+    , m_refreshRate(-1.0)
+    , m_scale(1.0)
+    , m_formFactor(mir_form_factor_unknown)
     , m_renderTarget(nullptr)
     , m_displayGroup(nullptr)
     , m_orientationSensor(new QOrientationSensor(this))
     , m_screenWindow(nullptr)
     , m_unityScreen(nullptr)
-    , m_formFactor(mir_form_factor_unknown)
-    , m_scale(1.0)
-    , m_refreshRate(-1.0)
 {
     setMirDisplayConfiguration(screen, false);
 
