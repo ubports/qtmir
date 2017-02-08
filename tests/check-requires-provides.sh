3cc9eb4660240aac43074e6b182a2c295ee89dfc
Albert Astals Cid <albert.astals@canonical.com>
2017-02-08 14:22:02 +0100

    Skip the test for the gles build

--- tests/check-requires-provides.sh
+++ tests/check-requires-provides.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# don't run the test on the gles build
+dummy=`grep qtmir-desktop debian/control`
+if [ $? -ne 0 ]; then
+    exit 0
+fi
+
 requires=`grep unity-shell-application= CMakeLists.txt | sed -E 's/.*=(.*)\)/\1/'`
 provides=`grep unity-application-impl- debian/control | sed -E 's/.*unity-application-impl-(.*),/\1/'`
 if [ "$requires" -eq "$provides" ]; then
