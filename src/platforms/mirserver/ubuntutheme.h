46005111a009c62b54a291e87b18d19029bf917b
Ratchanan Srirattanamet <ratchanan@ubports.com>
2020-04-29 03:18:24 +0700

    mirserver: uses UbuntuTheme from qtubuntu
    There's a discrepancy between the icon theme used in qtmir (i.e. Unity8/
    Lomiri) vs. the one used in qtubuntu (i.e. applications) and ubuntu-ui-
    toolkit (used for `image://theme/` uri).
    
    This commit copies the UbuntuTheme file from qtubuntu, which also brings
    in the font settings. This is intended to be a temporary solution; the
    better solution would be to let soon-to-be qtlomiri-appmenutheme expose
    this theme and re-rename it to qtlomiri-themes [1].
    
    This fixes the indicator-sound's microphone icons issue. ubuntu-mobile
    icon theme doesn't have the icon the indicator expects, causing it to
    failback to another icon, despite the fact that the icon will come from
    Suru anyway.
    
    [1] Or, better yet, maybe we shouldn't hardcode these things in and uses
    some other mechanism to allow these to be configurable? I don't know.

--- src/platforms/mirserver/ubuntutheme.h
+++ src/platforms/mirserver/ubuntutheme.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2015 Canonical, Ltd.
+ * Copyright (C) 2016-2017 Canonical, Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 3, as published by
@@ -14,22 +14,52 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef UBUNTU_THEME_H
-#define UBUNTU_THEME_H
-
-#include <QtGlobal>
-
+#include <QVariant>
 #include <QtThemeSupport/private/qgenericunixthemes_p.h>
 
 class UbuntuTheme : public QGenericUnixTheme
 {
 public:
-    static const char* name;
-    UbuntuTheme();
-    virtual ~UbuntuTheme();
+    static constexpr const char* name = "ubuntu";
 
-    // From QPlatformTheme
-    QVariant themeHint(ThemeHint hint) const override;
+    UbuntuTheme()
+      : mSystemFont(QStringLiteral("Ubuntu Regular"), 10),
+        mFixedFont(QStringLiteral("Ubuntu Mono Regular"), 13)
+    {
+        mSystemFont.setStyleHint(QFont::System);
+        mFixedFont.setStyleHint(QFont::TypeWriter);
+    }
+    ~UbuntuTheme() = default;
+
+    QVariant themeHint(ThemeHint hint) const override
+    {
+        switch (hint) {
+        case QPlatformTheme::SystemIconThemeName: {
+            QByteArray iconTheme = qgetenv("QTUBUNTU_ICON_THEME");
+            if (iconTheme.isEmpty()) {
+                return QStringLiteral("suru");
+            } else {
+                return iconTheme;
+            }
+        }
+        default:
+            break;
+        }
+        return QGenericUnixTheme::themeHint(hint);
+    }
+
+    const QFont *font(Font type) const override
+    {
+        switch (type) {
+        case QPlatformTheme::SystemFont:
+            return &mSystemFont;
+        case QPlatformTheme::FixedFont:
+            return &mFixedFont;
+        default:
+            return nullptr;
+        }
+    }
+
+private:
+    QFont mSystemFont, mFixedFont;
 };
-
-#endif // UBUNTU_THEME_H
