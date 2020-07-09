eb635ecc9291611cce8b2f0df672b7ef59c0c828
Marius Gripsgard <marius@ubports.com>
2020-07-10 00:05:02 +0200

    [TMP] Allow old type buffers

--- src/platforms/mirserver/miral/mirbuffer.cpp
+++ src/platforms/mirserver/miral/mirbuffer.cpp
@@ -18,19 +18,58 @@
 
 #include <mir/graphics/buffer.h>
 #include "mir/graphics/texture.h"
+#include <mir/renderer/gl/texture_source.h>
 
 #include <stdexcept>
 
+#include <QDebug>
+
 miral::GLBuffer::GLBuffer() = default;
-miral::GLBuffer::~GLBuffer() = default;
-miral::GLBuffer::GLBuffer(std::shared_ptr<mir::graphics::Buffer> const& buffer) :
-    wrapped(buffer)
+miral::GLBuffer::~GLBuffer()
 {
+    destroy();
+}
+
+miral::GLBuffer::GLBuffer(std::shared_ptr<mir::graphics::Buffer> const& buffer) :
+    wrapped(buffer),
+    m_textureId(0),
+    m_isOldTex(false)
+{
+    init();
+}
+
+void miral::GLBuffer::init()
+{
+    if (m_inited)
+        return;
+
+    if (!dynamic_cast<mir::graphics::gl::Texture*>(wrapped->native_buffer_base()))
+    {
+        qWarning("IS OLD TEX");
+        glGenTextures(1, &m_textureId);
+        m_isOldTex = true;
+        
+    } else {
+        qWarning("IS NEW TEX");
+    }
+
+    m_inited = true;
+}
+
+void miral::GLBuffer::destroy()
+{
+    qWarning("destroy");
+    if (m_textureId) {
+        glDeleteTextures(1, &m_textureId);
+        m_textureId = 0;
+        m_isOldTex = false;
+    }
 }
 
 void miral::GLBuffer::reset(std::shared_ptr<mir::graphics::Buffer> const& buffer)
 {
     wrapped = buffer;
+    init();
 }
 
 miral::GLBuffer::operator bool() const
@@ -55,14 +94,35 @@ void miral::GLBuffer::reset()
     wrapped.reset();
 }
 
+void miral::GLBuffer::gl_bind_tex()
+{
+    if (m_isOldTex)
+        glBindTexture(GL_TEXTURE_2D, m_textureId);
+}
+
 void miral::GLBuffer::bind()
 {
+    if (!wrapped) {
+        qWarning("Bind called without any buffers!");
+    }
+
+    if (m_isOldTex) {
+        auto const texsource = dynamic_cast<mir::renderer::gl::TextureSource*>(wrapped->native_buffer_base());
+
+        texsource->gl_bind_to_texture();
+        texsource->secure_for_render();
+     //   glPixelStorei(GL_UNPACK_ALIGNMENT, 4); 
+
+        return;
+    }
+
     if (auto const texture = dynamic_cast<mir::graphics::gl::Texture*>(wrapped->native_buffer_base()))
     {
         texture->bind();
     }
     else
     {
-        throw std::logic_error("Buffer does not support GL rendering");
+        qWarning("Bind called without any valid buffers!");
+        //throw std::logic_error("Buffer does not support GL rendering");
     }
 }
